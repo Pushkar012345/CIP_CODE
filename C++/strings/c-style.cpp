e89b139a7b87a6df8e97b23c485d599924208392
pushkarpawaroff <pushkarpawaroff@gmail.com>
2025-09-20 18:12:16 +0530

    c++->string->c-style-string

---
+++ C++/strings/c-style.cpp
@@ -0,0 +1,52 @@
+//strlen
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     char s1[]="gfg";
+//     char s2[]={'c','P','P','\0'};
+
+//     cout<<strlen(s1)<<endl;
+//     cout<<strlen(s2)<<endl;
+
+//     return 0;
+// }
+
+
+//strcmp
+
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+
+// int main() {
+//     char s1[] = "gfg";
+//     char s2[] = "abcd";
+
+//     int result = strcmp(s1, s2);
+
+//     if (result == 0) {
+//         cout << "Same" << endl;
+//     } else if (result < 0) {
+//         cout << "Smaller" << endl;
+//     } else {
+//         cout << "Greater" << endl; // Output: Greater
+//     }
+
+//     return 0;
+// }
+
+//strcpy
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     char str[5];
+//     strcpy(str,"gfg");
+
+//     cout<<str<<endl;
+//     return 0;
+// }
